5aa1910c6732f1d3c79fca2af12470a3e74ad9a0
mc8mac <marcosmachadomcm@gmail.com>
2023-10-27 13:58:46 +0100

    fixed tests

--- runtests.sh
+++ runtests.sh
@@ -3,17 +3,18 @@
 let total=0;
 let correct=0;
 
-for x in testsBoth/*.in; do
+for x in tests/*.in; do
     if [ -e ${x%.in}.import ]; then
-        java -cp :po-uilib.jar:. -Dimport=${x%.in}.import -Din=$x -DwriteInput=true -Dout=${x%.in}.outhyp xxl.app.App;
+        java -cp :po-uilib.jar:. -Dimport=${x%.in}.import -Din=$x -Dout=${x%.in}.outhyp xxl.app.App;
     else
-        java -cp po-uilib.jar:. -Din=$x -DwriteInput=true -Dout=${x%.in}.outhyp xxl.app.App;
+        java -cp po-uilib.jar:. -Din=$x -Dout=${x%.in}.outhyp xxl.app.App;
     fi
 
     diff -cwB ${x%.in}.out ${x%.in}.outhyp > ${x%.in}.diff ;
     if [ -s ${x%.in}.diff ]; then
         echo -n "F"
         failures=$failures"Fail: $x: See file ${x%.in}.diff\n" ;
+#        echo "FAIL: $x. See file ${x%.in}.diff " ;
     else
         let correct++;
         echo -n "."
