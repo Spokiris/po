45ae9510bbed9aecf82826ceedcb19c1fe67b8ce
mc8mac <marcos.machado@tecnico.ulisboa.pt>
2023-10-27 06:32:46 +0100

    doSaveFix

--- xxl-skeleton/xxl/app/main/DoSave.java
+++ xxl-skeleton/xxl/app/main/DoSave.java
@@ -3,9 +3,12 @@ package xxl.app.main;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import xxl.app.exception.FileOpenFailedException;
+import xxl.app.exception.UnknownFunctionException;
 import xxl.core.Calculator;
 import xxl.core.exception.MissingFileAssociationException;
 import pt.tecnico.uilib.menus.Command;
+import pt.tecnico.uilib.menus.CommandException;
 
 /**
  * Save to file under current name (if unnamed, query for name).
@@ -14,19 +17,24 @@ class DoSave extends Command<Calculator> {
 
   DoSave(Calculator receiver) {
     super(Label.SAVE, receiver, xxl -> xxl.getSpreadsheet() != null);
-    addStringField("filename","Introduza o nome do ficheiro:");
+    addStringField("filename",Message.openFile());
   }
   
   @Override
-  protected final void execute() {
-    if (_receiver.getFilename() == null) {
+  protected final void execute() throws CommandException{
       String filename = stringField("filename");
       try{
-      _receiver.saveAs(filename);
-      }catch (FileNotFoundException e){
-      }catch (IOException e){
-      }catch (MissingFileAssociationException e){
+        _receiver.save();
+        }catch (FileNotFoundException e){
+        try{
+          _receiver.saveAs(filename);
+        }catch (IOException e1){
+          throw new FileOpenFailedException(e1);
+        } catch (MissingFileAssociationException e1) {
+          throw new FileOpenFailedException(e1);
+        }
+      } catch (IOException e) {
+        throw new FileOpenFailedException(e);
       }
-    }
   }
 }
